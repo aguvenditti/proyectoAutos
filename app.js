e0f622844a7ded7746a05bba211f231b44cb391a
aguvenditti <aguvenditti@gmail.com>
2020-05-29 19:42:45 -0300

    agregue filtrado por marca y por anio

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 let moduloAutos = require('./autos.js');
 
-console.log(moduloAutos);
+// console.log(moduloAutos.porColor('black'));
+// console.log(moduloAutos.porMarca('volkswagen'));
+console.log(moduloAutos.porAnio(2015));
+
